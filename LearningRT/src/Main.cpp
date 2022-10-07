6b4271795e40da66eb52b534a6489f6999f8910a
Matt Ware <mattbware3@gmail.com>
2022-10-07 14:58:01 +0100

    Comments and camera controls

--- LearningRT/src/Main.cpp
+++ LearningRT/src/Main.cpp
@@ -3,10 +3,23 @@
 #include "Walnut/Application.h"
 #include "Walnut/EntryPoint.h" // EntryPoint must be included after Application
 #include "Walnut/Timer.h"
+#include "Walnut/Input/Input.h"
 
 class RaytracingLayer : public Walnut::Layer
 {
 public:
+	virtual void OnUpdate(float ts) override
+	{
+		if (Walnut::Input::IsKeyDown(Walnut::KeyCode::S))
+			m_Renderer.rayOrigin.y += m_MoveSpeed * ts;
+		if (Walnut::Input::IsKeyDown(Walnut::KeyCode::W))
+			m_Renderer.rayOrigin.y -= m_MoveSpeed * ts;
+		if (Walnut::Input::IsKeyDown(Walnut::KeyCode::A))
+			m_Renderer.rayOrigin.x += m_MoveSpeed * ts;
+		if (Walnut::Input::IsKeyDown(Walnut::KeyCode::D))
+			m_Renderer.rayOrigin.x -= m_MoveSpeed * ts;
+	}
+
 	virtual void OnUIRender() override
 	{
 		// Settings window
@@ -16,6 +29,12 @@ public:
 		ImGui::Checkbox("Keep Rendering", &m_KeepRendering);
 		ImGui::DragFloat3("Camera Position", &m_Renderer.rayOrigin[0], 0.1f);
 		ImGui::DragFloat("Sphere Radius", &m_Renderer.radius, 0.05f);
+		if (ImGui::Button("Reset"))
+		{
+			m_Renderer.rayOrigin = glm::vec3(0, 0, 2);
+			m_Renderer.radius = 0.5f;
+		}
+		ImGui::DragFloat("Move Speed", &m_MoveSpeed, 0.05f);
 		ImGui::Text("Viewport size is %ix%i (total pixels: %i)", m_ViewportWidth, m_ViewportHeight, m_ViewportWidth * m_ViewportHeight);
 		ImGui::Text("Last render took %.3fms", m_LastRenderTime);
 		ImGui::End();
@@ -50,6 +69,9 @@ private:
 	bool m_KeepRendering = false;
 	uint32_t m_ViewportWidth = 0;
 	uint32_t m_ViewportHeight = 0;
+
+	// Controls
+	float m_MoveSpeed = 2;
 
 	// Renderer
 	Renderer m_Renderer;
--- LearningRT/src/Renderer.cpp
+++ LearningRT/src/Renderer.cpp
@@ -26,15 +26,17 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 	float b = 2.0f * glm::dot(rayOrigin, rayDirection);
 	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
 	
+	// >= 0 is a hit, < 0 is a miss.
 	float discriminant = b * b - 4.0f * a * c;
 
 	if (discriminant >= 0)
 	{
+		// We hit the circle, color with a basic UV representation so it looks cool
 		uint8_t r = static_cast<uint8_t>((coord.x + 1) / 2 * 255.0f);
 		uint8_t g = static_cast<uint8_t>((coord.y + 1) / 2 * 255.0f);
 		return 0xFF000000 | g << 8 | r;
 	}
-	return 0xFF000000;
+	return 0xFF000000; // No hit; return black
 }
 
 void Renderer::OnResize(uint32_t width, uint32_t height)
@@ -42,12 +44,13 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 	if (m_FinalImage)
 	{
 		if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height)
-			return;
-		m_FinalImage->Resize(width, height); // This function checks if it even needs resizing for us
+			return; // Resize is unnessesary, return early so we don't bother resizing or recreating image data. 
+		m_FinalImage->Resize(width, height);
 	}
 	else
 		m_FinalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
 
+	// If the image resized, or if it never existed in the first place, we need to reallocate the CPU buffer for the image.
 	delete[] m_ImageData;
 	m_ImageData = new uint32_t[static_cast<uint64_t>(width) * height];
 }
